398dba907f4711bfe4cedc389d046f83f2e83adc
johnpiapian <johnpiapian@gmail.com>
2023-05-27 21:23:59 -0500

    Organize files

--- python/practice/printExponent.py
+++ python/practice/printExponent.py
@@ -1,3 +1,10 @@
+# This program prints the powers of the given number x times.
+# For example, if the given number is 3 and x is 4, then the program will print:
+# 0 = 1
+# 3 = 3
+# 3 x 3 = 9
+# 3 x 3 x 3 = 27
+
 def pow(i, n): # i -> integer, n -> the power, x^0 = 1
 	total = 1
 	for x in range(n):
@@ -14,25 +21,17 @@ def printLetter(i, n):
 			result = result + f'{i} × '
 
 	if not result:
-		# result = "0"
-		result = '1'
+		result = '0'
 		
 	return result
 
 
 def printPow(i, n):
 	for index in range(n):
-
 		letter = printLetter(i, index)
 		val = pow(i, index)
 
 		print(f'{letter} = {val}')
 
-def fact(n):
-	if n <= 1:
-		return 1
-	else:
-		return n * fact(n-1)
-
-printPow(7, 7)
-print(fact(10))
+print("Printing powers of the given n:")
+printPow(3, 5)
